0b5a20ea64c8c9a23ec53cd152708cf9414c963e
S <raghavendar.s@philips.com>
2024-11-30 01:44:34 +0530

    First Draft of code for the GCP Project

---
+++ Development/bigquery_create_table.sql
@@ -0,0 +1,16 @@
+create table `irctc_dwh.irctc_stream_tb`
+(
+  row_key string,
+  name string,
+  age int64,
+  email string,
+  join_date date,
+  last_login timestamp,
+  loyalty_points int64,
+  account_balance float64,
+  is_active bool,
+  inserted_at timestamp,
+  updated_at timestamp,
+  loyalty_status string,
+  account_age_days int64
+)
